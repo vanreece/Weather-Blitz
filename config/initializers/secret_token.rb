08fab4972f5be51062e83cbed8062bc093c886b4
Danny Krause <danny@carbonfive.com>
2012-02-08 16:15:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherBlitz::Application.config.secret_token = 'd0642f44af829d9c947366d41c0ba23c8faad9e2be83833db6b5c7baf6ad5dd5b927617fb7408abad1bed4348bfd1e2215e3701132e60f6df0a0bf77469015cb'
