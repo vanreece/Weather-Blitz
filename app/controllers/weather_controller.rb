5d25be30e7affe16eafd8bda90f77abf1aeb20db
Danny Krause <danny@carbonfive.com>
2012-03-08 09:13:56 -0800

    Redirect to nws for your location.
    Database updates to match heroku.

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,5 @@
 class WeatherController < ApplicationController
   def index
+    redirect_to "http://forecast.weather.gov/MapClick.php?lat=#{params[:lat]}&lon=#{params[:lon]}&site=mtr&unit=0&lg=en&FcstType=text"
   end
 end
